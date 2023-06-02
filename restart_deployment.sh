5cc8190e9deea4e0402354761cfa3b9ba347dfe9
Martin Willemsma <martin@willemsma.it>
2023-06-03 01:35:08 +0200

    be more verbose in output

--- restart_deployment.sh
+++ restart_deployment.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # Get deployment
 DEPLOYMENT=$(echo $HOSTNAME | sed '$s/-\w*$//g;$s/-\w*$//g')
+
 DEPLOYMENT_STATUS=$(kubectl rollout status deploy $DEPLOYMENT --watch=false)
 
-echo $DEPLOYMENT_STATUS | grep  'successfully rolled out'
+echo $DEPLOYMENT_STATUS | grep 'successfully rolled out'
 if [ $? == 0 ]; then
-    echo "Restarting deployment"
+    echo "Restarting deployment ${DEPLOYMENT}"
     kubectl rollout restart deploy $DEPLOYMENT
 else
-    echo "Rollout already inprogress. Skipping"
+    echo "Rollout already in progress. Skipping"
+    echo "${$DEPLOYMENT_STATUS}"
 fi
