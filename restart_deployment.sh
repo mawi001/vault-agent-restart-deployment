d289a045160fa66a2a5002673be1cfee27f7d7dc
Martin Willemsma <martin@willemsma.it>
2023-06-03 01:46:16 +0200

    add deployent status

--- restart_deployment.sh
+++ restart_deployment.sh
@@ -6,9 +6,8 @@ DEPLOYMENT_STATUS=$(kubectl rollout status deploy $DEPLOYMENT --watch=false)
 
 echo $DEPLOYMENT_STATUS | grep 'successfully rolled out'
 if [ $? == 0 ]; then
-    echo "Restarting deployment ${DEPLOYMENT}"
     kubectl rollout restart deploy $DEPLOYMENT
 else
     echo "Rollout already in progress. Skipping"
-    echo "${$DEPLOYMENT_STATUS}"
+    echo $DEPLOYMENT_STATUS
 fi
