bf659e6e736b9504fa7a99e7a8bd27a20abeab19
Martin Willemsma <martin@willemsma.it>
2023-06-03 01:57:44 +0200

    bad substitution

--- restart_deployment.sh
+++ restart_deployment.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Get deployment
 DEPLOYMENT=$(echo $HOSTNAME | sed '$s/-\w*$//g;$s/-\w*$//g')
 
 DEPLOYMENT_STATUS=$(kubectl rollout status deploy $DEPLOYMENT --watch=false)
@@ -9,5 +8,5 @@ if [ $? == 0 ]; then
     kubectl rollout restart deploy $DEPLOYMENT
 else
     echo "Rollout already in progress. Skipping"
-    echo $DEPLOYMENT_STATUS
+    echo "${DEPLOYMENT_STATUS}"
 fi
